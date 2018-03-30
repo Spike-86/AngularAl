73085927471526219a8c2eebf0596c915a9991e2
tropynin <Trapynin@alutech-mc-com>
2018-03-30 17:07:46 +0300

    Добавили сообщения

---
+++ src/app/messages.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+
+export enum Typemsg {INFO, ERROR, WARNING}
+
+
+interface IMessage {
+  text: string;
+  type: Typemsg;
+}
+
+@Injectable()
+export class MessagesService {
+
+  private messages: Array<IMessage> = [];
+  private countViewMsg = 10;
+
+  constructor() { }
+
+  addMessage(msg: IMessage) {
+
+    this.messages.push(msg);
+
+  }
+
+  getMessages(): Array<IMessage> {
+
+    return this.messages;
+  }
+
+}
